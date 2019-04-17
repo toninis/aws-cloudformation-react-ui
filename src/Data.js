ac4691c32ab4e2388515250268f170618ecb8211
Antonis Stamatiou <a.stamatiou@encodegroup.com>
2019-04-17 16:00:00 +0300

    Tweaking ui

--- src/Data.js
+++ src/Data.js
@@ -1,9 +1,23 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import ExpansionPanel from "@material-ui/core/ExpansionPanel";
 import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
 import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Collapse from "@material-ui/core/Collapse";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import ExpandLess from "@material-ui/icons/ExpandLess";
+import ExpandMore from "@material-ui/icons/ExpandMore";
+import StarBorder from "@material-ui/icons/StarBorder";
+
+import "./Data.css";
 
 const styles = theme => ({
   root: {
@@ -25,11 +39,18 @@ class Data extends Component {
     };
   }
 
+  state = {
+    open: true
+  };
+
+  handleClick = () => {
+    this.setState(state => ({ open: !state.open }));
+  };
+
   componentDidMount() {
     fetch("http://localhost:4000")
       .then(response => response.json())
       .then(data => {
-        console.log(data);
         this.setState({
           ...this.state,
           summaries: data.StackSummaries,
@@ -40,56 +61,92 @@ class Data extends Component {
 
   render() {
     const summaries = this.state.summaries;
-    console.log(this.state);
+    // console.log(this.state);
     return (
       <div className="Data">
         <ExpansionPanel>
-          <ExpansionPanelSummary expandIcon={"+"}>
-            <h3>DELETE_COMPLETE</h3>
+          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+            <h3>Deleted Demos</h3>
           </ExpansionPanelSummary>
-          <div>
+          <List component="nav" className="Data">
             {summaries
               .filter(s => s.StackStatus === "DELETE_COMPLETE")
               .map((s, i) => {
-                return <li>{s.StackName}</li>;
+                // return <div className="demo"> {s.StackName} </div>;
+                return (
+                  <React.Fragment>
+                    <ListItem button onClick={this.handleClick}>
+                      <ListItemIcon>
+                        <InboxIcon />
+                      </ListItemIcon>
+                      <ListItemText inset primary={s.StackName} />
+                      {this.state.open ? <ExpandLess /> : <ExpandMore />}
+                    </ListItem>
+                    <Collapse in={this.state.open} timeout="auto" unmountOnExit>
+                      <List component="div" disablePadding>
+                        <ListItem button className="Data">
+                          <ListItemIcon>
+                            <StarBorder />
+                          </ListItemIcon>
+                          <ListItemText inset primary="Starred" />
+                        </ListItem>
+                      </List>
+                    </Collapse>
+                  </React.Fragment>
+                );
               })}
-          </div>
+          </List>
         </ExpansionPanel>
         <ExpansionPanel>
-          <ExpansionPanelSummary expandIcon={"+"}>
-            <h3>CREATE_COMPLETE</h3>
+          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+            <h3>Running Demos</h3>
           </ExpansionPanelSummary>
-          <div>
-            {summaries
-              .filter(s => s.StackStatus === "CREATE_COMPLETE")
-              .map((s, i) => {
-                return <li>{s.StackName}</li>;
-              })}
-          </div>
         </ExpansionPanel>
+        <List component="nav" className="Data">
+          {summaries
+            .filter(s => s.StackStatus === "CREATE_COMPLETE")
+            .map((s, i) => {
+              // return <div className="demo"> {s.StackName} </div>;
+              return (
+                <React.Fragment>
+                  <ListItem button onClick={this.handleClick}>
+                    <ListItemIcon>
+                      <InboxIcon />
+                    </ListItemIcon>
+                    <ListItemText inset primary={s.StackName} />
+                    {this.state.open ? <ExpandLess /> : <ExpandMore />}
+                  </ListItem>
+                  <Collapse in={this.state.open} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                      <ListItem button className="Data">
+                        <ListItemIcon>
+                          <StarBorder />
+                        </ListItemIcon>
+                        <ListItemText inset primary="Starred" />
+                      </ListItem>
+                    </List>
+                  </Collapse>
+                </React.Fragment>
+              );
+            })}
+        </List>
         <ExpansionPanel>
-          <ExpansionPanelSummary expandIcon={"+"}>
-            <h3>CREATE_FAILED</h3>
+          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+            <h3>Failed Demos</h3>
           </ExpansionPanelSummary>
-          <div>
-            {summaries
-              .filter(s => s.StackStatus === "CREATE_FAILED")
-              .map((s, i) => {
-                return <li>{s.StackName}</li>;
-              })}
-          </div>
-        </ExpansionPanel>
-        <ExpansionPanel>
-          <ExpansionPanelSummary expandIcon={"+"}>
-            <h3>DELETE_FAILED</h3>
-          </ExpansionPanelSummary>
-          <div>
-            {summaries
-              .filter(s => s.StackStatus === "DELETE_FAILED")
-              .map((s, i) => {
-                return <li>{s.StackName}</li>;
-              })}
-          </div>
+          <ExpansionPanelDetails>
+            <Typography>
+              {summaries
+                .filter(
+                  s =>
+                    s.StackStatus === "CREATE_FAILED" ||
+                    s.StackStatus === "DELETE_FAILED"
+                )
+                .map((s, i) => {
+                  return <div className="demo"> {s.StackName} </div>;
+                })}
+            </Typography>
+          </ExpansionPanelDetails>
         </ExpansionPanel>
       </div>
     );
